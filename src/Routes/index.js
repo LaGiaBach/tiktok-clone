407167114d25106be097536fcc25ae989ff27b40
LaGiaBach <lagiabach1610@gmail.com>
2022-07-16 18:56:21 +0700

    Update styles header,sidebar and content

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,6 +4,7 @@ import Home from '../pages/Home';
 import Following from '../pages/Following';
 import Profile from '../pages/Profile';
 import Upload from '../pages/Upload';
+import Search from '../pages/Search';
 
 //Can access only when login
 const publicRoutes = [
@@ -23,6 +24,11 @@ const publicRoutes = [
     path: '/upload',
     component: Upload,
     layout: HeaderOnly
+  },
+  {
+    path: '/search',
+    component: Search,
+    layout: null
   }
 ];
 //Need login to access, if it not, return to login page
